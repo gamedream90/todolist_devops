a1ea65f2ec0f3eb6729b1c08ce4665204167ab59
gamedream90 <nidhinpaul@karunya.edu.in>
2025-10-29 18:09:35 +0530

    Initial commit after re-initialization

---
+++ todolist/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# In-memory "database"
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form.get('todo')
+    if todo:
+        todos.append(todo)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
